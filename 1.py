9851896152cf3125420574ecd75fb3ccbecb36b1
shellCI <95949537+shellCI@users.noreply.github.com>
2021-12-11 14:29:09 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,11 @@
 # -*- coding: UTF-8 -*-
 import requests as req
 import json,sys,time
-#由@蝶铃魔改 仅供自用
+#先注册azure应用,确保应用有以下权限:
+#files:	Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
+#user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
+#mail:  Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
+#注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用
 
 
 
